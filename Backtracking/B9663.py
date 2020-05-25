32d62eb4ad91a365c4e903a44b166a6488f0e91e
KimTaewoo <xass1995@gmail.com>
2020-05-25 23:49:11 +0900

    update problem solving of backtracking

---
+++ Backtracking/B9663.py
@@ -0,0 +1,26 @@
+# BOJ 문제
+# N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
+# N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
+
+# 첫째 줄에 N이 입력 (1 ≤ N < 15)
+# 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력
+
+n, ans = int(input()), 0
+a, b, c = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
+
+def solve(i):
+    global ans
+    if i == n:
+        ans += 1
+        return
+    for j in range(n):
+        if not (a[j] or b[i+j] or c[i-j+n-1]):
+            a[j] = b[i+j] = c[i-j+n-1] = True
+            solve(i+1)
+            a[j] = b[i+j] = c[i-j+n-1] = False
+
+solve(0)
+print(ans)
+
+
+# 출처: https://rebas.kr/761
