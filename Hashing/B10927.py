b3879cd2bd102f24435a70256a4c3a2c5547467f
KimTaewoo <xass1995@gmail.com>
2020-07-08 17:05:06 +0900

    update problem solving of hashing

---
+++ Hashing/B10927.py
@@ -0,0 +1,15 @@
+# BOJ 문제
+# 문자열 s가 주어졌을 때, MD5 해시값을 구하는 프로그램을 작성하시오.
+
+# 첫째 줄에 문자열 s를 입력
+# s는 알파벳 대문자와 소문자, 그리고 숫자로만 이루어져 있으며, 길이는 최대 50
+# 첫째 줄에 s의 MD5 해시값을 출력
+
+import hashlib
+
+string = str(input())
+
+encode_string = string.encode()
+md5_string = hashlib.md5(encode_string).hexdigest()
+
+print(md5_string)
