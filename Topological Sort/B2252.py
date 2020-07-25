34094cc8c404fc55f9f29613fb0909937ef39b8b
KimTaewoo <xass1995@gmail.com>
2020-07-25 11:17:09 +0900

    update problem solving of topological sort

---
+++ Topological Sort/B2252.py
@@ -0,0 +1,53 @@
+# BOJ 문제
+# N명의 학생들을 키 순서대로 줄을 세우려고 한다.
+# 각 학생의 키를 직접 재서 정렬하면 간단하겠지만, 마땅한 방법이 없어서 두 학생의 키를 비교하는 방법을 사용하기로 하였다.
+# 그나마도 모든 학생들을 다 비교해 본 것이 아니고, 일부 학생들의 키만을 비교해 보았다.
+# 일부 학생들의 키를 비교한 결과가 주어졌을 때, 줄을 세우는 프로그램을 작성하시오.
+
+# 첫째 줄에 N(1≤N≤32,000), M(1≤M≤100,000)이 주어진다.
+# M은 키를 비교한 회수이다.
+# 다음 M개의 줄에는 키를 비교한 두 학생의 번호 A, B가 주어진다.
+# 이는 학생 A가 학생 B의 앞에 서야 한다는 의미이다.
+
+# 학생들의 번호는 1번부터 N번이다.
+
+# 첫째 줄부터 앞에서부터 줄을 세운 결과를 출력한다.
+# 답이 여러 가지인 경우에는 아무거나 출력한다.
+
+num_student, num_compare = map(int, input().split())
+graph_list = []
+student_list = [[] for i in range(num_student + 1)]
+indegree = [0 for i in range(num_student + 1)]
+queue = []
+result = []
+
+#Graph Information
+for i in range(num_compare):
+    graph = list(map(int, input().split()))
+    graph_list.append(graph)
+
+#Node & Indegree Information
+for [i, j] in graph_list:
+    student_list[i].append(j)
+    indegree[j] += 1
+  
+#Make First Queue
+for i in range(1, num_student + 1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+#Make Topological Sort Loop
+while queue:
+    for i in queue:
+        temp = i
+        queue.remove(i)
+        result.append(temp)
+        for j in student_list[temp]:
+            indegree[j] -= 1
+            if indegree[j] == 0:
+                queue.append(j)
+        
+for i in result:
+    print(i, end=' ')
+
+# 출처: https://claude-u.tistory.com/117
